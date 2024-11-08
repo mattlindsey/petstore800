c2970da0fa631f2f9e465a26745b835715dc8271
Matt Lindsey <matthew.r.lindsey@gmail.com>
2024-11-08 09:52:45 -0500

    Fixes for rubocop and config

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,10 +4,10 @@ class HomeController < ApplicationController
     @pictures = Picture.all
     @about_matt = {
       biography: "Matt is a software developer with a passion for technology.",
-      hobbies: ["Coding", "Reading", "Hiking"],
-      achievements: ["Built a successful app", "Published a book"],
+      hobbies: [ "Coding", "Reading", "Hiking" ],
+      achievements: [ "Built a successful app", "Published a book" ],
       contact_email: "matt@example.com",
-      social_media_links: ["https://twitter.com/matt", "https://github.com/matt"]
+      social_media_links: [ "https://twitter.com/matt", "https://github.com/matt" ]
     }
     render :index
   end
